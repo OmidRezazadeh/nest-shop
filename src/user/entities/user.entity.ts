5078f6a80184918965359f1691b6b82d612506cc
orprogrammer <orprogrammer@gmail.com>
2025-03-07 00:48:50 +0330

    Update User entity with additional fields and timestamps

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,20 +1,36 @@
+import { profile } from "console";
+import { Profile } from "src/profile/entities/profile";
 import { Role } from "src/role/entities/role.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('users')
 export class User{
     @PrimaryGeneratedColumn()
     id:number;
 
-   @Column({unique:true})
-   email:string
+  @Column({unique:true})
+  email:string
 
   @Column()
   password: string;
 
+  @Column()
+  status:string;
+  
+  @Column()
+  phone:string;
 
   @ManyToOne(()=>Role,(role)=>role.users)
   role:Role;
   
+  @OneToOne(()=> Profile,(profile)=>profile.user)
+  profile:Profile; 
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
 
 }
