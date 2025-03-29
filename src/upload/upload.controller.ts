b10f46f53b77f07926f40434217d5f54979c5bd2
orprogrammer <orprogrammer@gmail.com>
2025-03-29 22:42:30 +0330

    refactor(upload): Improve upload controller with dynamic configuration and cleanup
    - Refactored the UploadController to utilize environment variables for file size, image width, and quality settings.
    - Cleaned up commented-out code for better readability and maintainability.
    - Updated the file path for uploaded images to use a configurable upload directory.

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -5,49 +5,31 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as sharp from 'sharp';
 import { FileSizeValidationPipe } from 'src/pipes/file-validation';
-
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
 
-  // @Post()
-  // @UseInterceptors(
-  //   FileInterceptor('file', {
-  //     storage: diskStorage({
-  //       destination: 'uploads/',
-  //       filename: (req, file, cb) => {
-  //         cb(null, `${Date.now()}-${file.originalname}`);
-  //       },
-  //     }),
-  //     fileFilter: (req, file, cb) => {
-  //       if (!file.mimetype.match(/(jpg|jpeg|png)$/)) {
-  //         return cb(new Error('Invalid file type!'), false);
-  //       }
-  //       cb(null, true);
-  //     },
-  //   }),
-  // )
-  // async uploadFile(
-  //   @UploadedFile() file: Express.Multer.File,
-  //   @Body() uploadDto: UploadDto,
-  // ) {
-  //   return this.uploadService.saveFile(file, uploadDto.imageable_id, uploadDto.imageable_type);
-  // }
+  constructor(
+    private readonly uploadService: UploadService
+  ) {
+  }
+
 
   @Post('file')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
-    @UploadedFile(new FileSizeValidationPipe(200000)) file: Express.Multer.File){
+    @UploadedFile(
+      new FileSizeValidationPipe( Number(process.env.MAX_FILE_SIZE))) file: Express.Multer.File
+    ){
       const fileName = `compressed-${Date.now()}.jpg`;
-      const filePath=path.join(__dirname, '../../public/temps',fileName);
+      const filePath=path.join(__dirname, '../../'+ process.env.UPLOAD_DIR,fileName);
        await sharp(file.buffer)
-       .resize({width:800})
-       .jpeg({ quality: 80 })
+       .resize({width: Number(process.env.IMAGE_WIDTH) })
+       .jpeg({ quality: Number(process.env.IMAGE_QUALITY) })
        .toFile(filePath);
 
     return {
       message: ' عکس با موفقیت اپلود شد',
-      fileUrl: `/public/temps/${fileName}`, // Correct URL for frontend
+      fileUrl: `/${process.env.UPLOAD_DIR}/${fileName}`
     };
   }
 
