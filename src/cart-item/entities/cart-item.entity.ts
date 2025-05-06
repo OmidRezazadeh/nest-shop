90d9ad1444401d31df40e282cc8d63416f68f4e2
orprogrammer <orprogrammer@gmail.com>
2025-05-07 00:40:47 +0330

    feat(cart-item): Implement CartItem module with controller, service, and entity
    - Added CartItemController and CartItemService to manage cart item operations.
    - Created CartItemModule to encapsulate cart item-related functionality.
    - Introduced CartItem entity for database representation and relationships with Cart and Product entities.
    - Developed unit tests for CartItemController and CartItemService to ensure proper functionality.
    - Added CartItemDto for future input validation.

---
+++ src/cart-item/entities/cart-item.entity.ts
@@ -0,0 +1,39 @@
+import { Cart } from 'src/cart/entities/cart.entity';
+import { Product } from 'src/product/entities/product.entity';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    CreateDateColumn,
+    JoinColumn,
+    UpdateDateColumn,
+  } from 'typeorm';
+
+  @Entity('cart_item')
+  export class CartItem{
+    @PrimaryGeneratedColumn()
+    id: string;
+    @ManyToOne(()=> Cart,(cart)=> cart.items, {onDelete:'CASCADE'})
+    @JoinColumn({name:'cart_id'})
+    cart:Cart;
+
+    @ManyToOne(()=> Product,{eager:true})
+    @JoinColumn({name:'product_id'})
+    product:Product;
+
+    
+    @Column({type:"decimal", precision:10,scale:2})
+    price:number;
+
+
+    @Column({ type: 'int' })
+    quantity: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @UpdateDateColumn()
+    updated_at: Date;
+  }
+
