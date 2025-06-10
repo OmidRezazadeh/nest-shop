91ef09b366909d98a1e2b7f95c89aa5ade98b4a9
orprogrammer <orprogrammer@gmail.com>
2025-06-10 21:21:11 +0330

    feat(order-item): Add create method to OrderItemController for order item creation

--- src/order-item/order-item.controller.ts
+++ src/order-item/order-item.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('order-item')
 export class OrderItemController {
 
+    @Post('crete')
+async create(){
+    
+}
     
 }
