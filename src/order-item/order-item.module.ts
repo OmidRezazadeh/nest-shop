582257d8d7b645ae13bab1fbc9a0c82ffbbd3983
orprogrammer <orprogrammer@gmail.com>
2025-06-10 21:22:31 +0330

    feat(order-item): Register JwtModule asynchronously in OrderItemModule for JWT configuration

--- src/order-item/order-item.module.ts
+++ src/order-item/order-item.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { OrderItemService } from './order-item.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/product/entities/product.entity';
+import { Order } from 'src/order/entities/order';
+import { OrderItem } from './entities/order-item';
+import { JwtModule } from '@nestjs/jwt';
+import { OrderItemController } from './order-item.controller';
 
 @Module({
-  providers: [OrderItemService]
+  imports:[TypeOrmModule.forFeature([Product,Order,OrderItem]),
+  JwtModule.registerAsync({
+    useFactory: () => ({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN 
+      },
+  }),
+}),
+],
+  controllers: [OrderItemController], 
+  providers: [OrderItemService],
+  exports: [TypeOrmModule]
 })
 export class OrderItemModule {}
