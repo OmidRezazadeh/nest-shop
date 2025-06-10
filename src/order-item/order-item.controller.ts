0b309f787acd92ce46cdcc0765d3022342c52cac
orprogrammer <orprogrammer@gmail.com>
2025-06-10 21:22:19 +0330

    feat(order-item): Implement createOrderItem method with JWT authentication and validation

--- src/order-item/order-item.controller.ts
+++ src/order-item/order-item.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { CreateOrderItemDto } from './dto/create-order-item.dto';
+import { OrderItemService } from './order-item.service';
+import { JwtAuthGuard } from 'src/guards/jwt-auth/jwt-auth.guard';
 
 @Controller('order-item')
 export class OrderItemController {
+ constructor(
+    private readonly orderItemService:OrderItemService
+ ){}
 
-    @Post('crete')
-async create(){
+
+
+@UseGuards(JwtAuthGuard)
+@Post('create')
+async createOrderItem(@Body() createOrderItemDto: CreateOrderItemDto,  @Request() request){
+     
+      // Extract user ID from the authenticated request
+      const userId = request.user.id;
+      await this.orderItemService.validate(createOrderItemDto,userId)
+      return await this.orderItemService.create(createOrderItemDto)  
     
 }
     
