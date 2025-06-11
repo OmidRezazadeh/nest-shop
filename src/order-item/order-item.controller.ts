95a5d252cccfe9beeca8a9592b2def1dcb9cb4c4
orprogrammer <orprogrammer@gmail.com>
2025-06-11 19:07:53 +0330

    feat(order-item): Add list method with role-based access control and update imports

--- src/order-item/order-item.controller.ts
+++ src/order-item/order-item.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Request, UseGuards } from '@nestjs/common';
 import { CreateOrderItemDto } from './dto/create-order-item.dto';
 import { OrderItemService } from './order-item.service';
 import { JwtAuthGuard } from 'src/guards/jwt-auth/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { ROLE_NAME } from '../common/constants/role-name';
+import { ListOrderItemDto } from './dto/list-order-item.dto';
 
 @Controller('order-item')
 export class OrderItemController {
@@ -14,12 +17,16 @@ export class OrderItemController {
 @UseGuards(JwtAuthGuard)
 @Post('create')
 async createOrderItem(@Body() createOrderItemDto: CreateOrderItemDto,  @Request() request){
-     
-      // Extract user ID from the authenticated request
       const userId = request.user.id;
       await this.orderItemService.validate(createOrderItemDto,userId)
       return await this.orderItemService.create(createOrderItemDto)  
     
+}
+@Roles(ROLE_NAME.Admin)
+@UseGuards(JwtAuthGuard)
+@Get('list')
+async getItemsByOrderId(@Query() listOrderItemDto: ListOrderItemDto){
+     return  await this.orderItemService.list(listOrderItemDto)
 }
     
 }
