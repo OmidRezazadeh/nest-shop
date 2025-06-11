439d4a74fdf55e2094b9ffd5b24d7e8b31d5c956
orprogrammer <orprogrammer@gmail.com>
2025-06-11 19:08:07 +0330

    refactor(order-item): Improve formatting and consistency in OrderItem entity

--- src/order-item/entities/order-item.ts
+++ src/order-item/entities/order-item.ts
@@ -1,21 +1,34 @@
-import { Order } from "src/order/entities/order";
-import {  Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Order } from 'src/order/entities/order';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+} from 'typeorm';
 import { Product } from '../../product/entities/product.entity';
 
 @Entity()
-export class OrderItem{
- @PrimaryGeneratedColumn()
- id:number;
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
 
- @ManyToOne(() => Order, order => order.items)
- order: Order;
+  @ManyToOne(() => Order, (order) => order.items)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+  @RelationId((item: OrderItem) => item.order)
+  orderId: number;
 
- @ManyToOne(()=> Product, product=>product.orderItems)
- product:Product;
+  @ManyToOne(() => Product, (product) => product.orderItems)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+  @RelationId((item: OrderItem) => item.product)
+  productId: number;
 
- @Column()
- quantity:number;
+  @Column()
+  quantity: number;
 
- @Column()
- price:number;
+  @Column()
+  price: number;
 }
