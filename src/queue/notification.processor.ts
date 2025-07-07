b65d170c6dd59c5c8489bc9fa72d80fb65eb5c65
orprogrammer <orprogrammer@gmail.com>
2025-07-07 15:41:39 +0330

    feat(notification): refactor NotificationProcessor to improve structure and readability
    - Updated constructor to use ModuleRef for better dependency management
    - Enhanced process method to streamline email sending logic
    - Added event handlers for job completion and failure with logging

--- src/queue/notification.processor.ts
+++ src/queue/notification.processor.ts
@@ -1,26 +1,27 @@
- import { OnWorkerEvent, Processor, WorkerHost  } from "@nestjs/bullmq";
- import { Job } from 'bullmq';
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from 'bullmq';
 import { MailService } from '../email/email.service';
+import { ModuleRef } from '@nestjs/core';
 @Processor('email-queue')
+export class NotificationProcessor extends WorkerHost {
+  constructor(
+    private moduleRef: ModuleRef,
+    private readonly mailService: MailService,
+  ) {
+    super();
+  }
+  async process(job: Job<{ email: string; price: number }>): Promise<any> {
+    const { email, price } = job.data;
 
-export class NotificationProcessor extends WorkerHost{
+    await this.mailService.sendOrderCreatedEmail(email, price);
+  }
 
-    constructor(private readonly mailService:MailService){
-        super();
-    }
-    async process(job: Job<{ email: string ,price:number}>): Promise<any> {
-         const { email,price } = job.data;
-
-         
-               await this.mailService.sendOrderCreatedEmail(email,price)
-    }
-    
-    @OnWorkerEvent('completed')
-    onCompleted(job: Job){
-        console.log(`Job ${job.id} completed! email-queue `);
-    }
-    @OnWorkerEvent('failed')
-    OnFailed(job:Job, error:Error){
-       console.error(`❌ Job ${job.id} failed with error:`, error.message);
-    }
+  @OnWorkerEvent('completed')
+  onCompleted(job: Job) {
+    console.log(`Job ${job.id} completed! email-queue `);
+  }
+  @OnWorkerEvent('failed')
+  OnFailed(job: Job, error: Error) {
+    console.error(`❌ Job ${job.id} failed with error:`, error.message);
+  }
 }
