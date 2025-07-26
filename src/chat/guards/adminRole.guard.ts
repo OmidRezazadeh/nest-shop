6fd6626e287baf8a572bd736365f0d79bb4a20f3
orprogrammer <orprogrammer@gmail.com>
2025-07-26 22:22:41 +0330

    refactor(chat): improve AdminRolesGuard for enhanced user validation
    - Refactored AdminRolesGuard to streamline user validation logic.
    - Added a check for user ID presence before querying the database.
    - Improved error messages for better clarity in case of access denial.
    - Enhanced code readability by organizing import statements and formatting.

--- src/chat/guards/adminRole.guard.ts
+++ src/chat/guards/adminRole.guard.ts
@@ -1,38 +1,40 @@
 import {
-    Injectable,
-    CanActivate,
-    ExecutionContext,
-    ForbiddenException,
-  } from '@nestjs/common';
-  import { Reflector } from '@nestjs/core';
-  import { Socket } from 'socket.io';
-import { ROLE_NAME } from 'src/common/constants/role-name';
-import { User } from '../../user/entities/user.entity';
-import { Repository } from 'typeorm';
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Socket } from 'socket.io';
 
-  
-  @Injectable()
-  export class AdminRolesGuard implements CanActivate {
-    constructor(
-      @InjectRepository(User)
-      private  readonly  userRepository:Repository<User>) {}
-  
-   async canActivate(context: ExecutionContext) {
-      
-      const client = context.switchToWs().getClient<Socket>();
-    
-      const user = await this.userRepository.findOne({where:{id:client.data.id},
-      relations:['role']
-      })
-  
- 
-  
-      if (!user || user.role.id !== ROLE_NAME.Admin) {
-        throw new ForbiddenException('شما اجازه دسترسی ندارید');
-      }
-  
-      return true;
+import { User } from '../../user/entities/user.entity';
+import { ROLE_NAME } from 'src/common/constants/role-name';
+
+@Injectable()
+export class AdminRolesGuard implements CanActivate {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const client = context.switchToWs().getClient<Socket>();
+    const userId = client?.data?.id;
+
+    if (!userId) {
+      throw new ForbiddenException('شناسه کاربر یافت نشد');
     }
+
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['role'],
+    });
+
+    if (!user || user.role.id !== ROLE_NAME.Admin) {
+      throw new ForbiddenException('شما اجازه دسترسی ندارید');
+    }
+
+    return true;
   }
-  
+}
