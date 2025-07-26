5365cd0296856d98af10b4ad396ed6414280d8aa
orprogrammer <orprogrammer@gmail.com>
2025-07-26 22:41:29 +0330

    fix(jwt-auth): update success and error messages to Persian translation
    - Changed the success message for authenticated users and the error message for invalid tokens in WsJwtGuard from English to Persian for improved localization.

--- src/chat/guards/auth.guard.ts
+++ src/chat/guards/auth.guard.ts
@@ -6,42 +6,43 @@ import { Socket } from 'socket.io';
 const BEARER_PREFIX = 'Bearer ';
 
 @Injectable()
-export class WsJwtGuard implements CanActivate{
-    private readonly logger = new Logger(WsJwtGuard.name)
+export class WsJwtGuard implements CanActivate {
+  private readonly logger = new Logger(WsJwtGuard.name);
 
-    constructor(private readonly jwtService:JwtService){}
-    canActivate(context: ExecutionContext): boolean  {
-        
-        const client:Socket = context.switchToWs().getClient();
-        const token =this.extractToken(client)
- 
-        if (!token) {
-            this.logger.error('No token provided')
-            throw new WsException('No token provided')
-        }
-        try {
-            const decoded= this.jwtService.verify(token,{
-                secret:process.env.JWT_SECRET
-            });
-            client.data.id = decoded.sub || decoded.id; 
-           this.logger.log(`Authenticated socket user: ${decoded.sub || decoded.id}`);
-           return true;
-        } catch (error) {
-            console.log(error)
-            this.logger.error(`Invalid WebSocket token: ${error.message}`);
-            throw new WsException('Invalid or expired token');
-        }
-        
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const client: Socket = context.switchToWs().getClient();
+    const token = this.extractToken(client);
+
+    if (!token) {
+      this.logger.error('توکن ارائه نشده است');
+      throw new WsException('برای اتصال باید وارد حساب کاربری شوید');
     }
-    private extractToken(client: Socket): string | null {
-        const tokenFromQuery = client.handshake.query['Authorization'];
-        const tokenFromHeaders = client.handshake.headers['authorization'];
-    
-        const token = Array.isArray(tokenFromQuery)
-          ? tokenFromQuery[0]?.split(BEARER_PREFIX)[1]
-          : tokenFromQuery?.split(BEARER_PREFIX)[1] ||
-            tokenFromHeaders?.split(BEARER_PREFIX)[1];
-    
-        return token || null;
-      }
+
+    try {
+      const decoded = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      client.data.id = decoded.sub || decoded.id;
+      this.logger.log(`کاربر با شناسه ${decoded.sub || decoded.id} با موفقیت احراز هویت شد`);
+      return true;
+    } catch (error) {
+      this.logger.error(`توکن نامعتبر یا منقضی شده است: ${error.message}`);
+      throw new WsException('دسترسی نامعتبر است، لطفاً دوباره وارد شوید');
+    }
+  }
+
+  private extractToken(client: Socket): string | null {
+    const tokenFromQuery = client.handshake.query['Authorization'];
+    const tokenFromHeaders = client.handshake.headers['authorization'];
+
+    const token = Array.isArray(tokenFromQuery)
+      ? tokenFromQuery[0]?.split(BEARER_PREFIX)[1]
+      : tokenFromQuery?.split(BEARER_PREFIX)[1] ||
+        tokenFromHeaders?.split(BEARER_PREFIX)[1];
+
+    return token || null;
+  }
 }
