4fe8bd7873d11342435da35dc182a031fec73ae4
orprogrammer <orprogrammer@gmail.com>
2025-08-01 20:44:21 +0330

    refactor(auth): remove unused import from ConfirmDto for improved code clarity

--- src/auth/dto/confirmDto.ts
+++ src/auth/dto/confirmDto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsNumber  } from "class-validator";
+import { IsEmail, IsNotEmpty  } from "class-validator";
 export class ConfirmDto{
     @IsNotEmpty({ message: 'ایمیل نمی‌تواند خالی باشد' })
     @IsEmail({}, { message: 'ایمیل وارد شده صحیح نیست ' })
